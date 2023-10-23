cd42c57d5283ccd69877a2e67f7cf37b3466d3ee
Щеколдин Максим Павлович <ShchekoldinMP@trasko.local>
2023-10-23 13:13:21 +0300

    add components and styles: app, app-info, search-panel

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+
+
+const SearchPanel = () => {
+  return (
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="Найти сотрудника"
+    />
+  );
+}
+
+export default SearchPanel;
