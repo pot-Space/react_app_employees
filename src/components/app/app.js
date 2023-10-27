8895fabf1a910bac5968b69fb6d9c1d905471010
Щеколдин Максим Павлович <ShchekoldinMP@trasko.local>
2023-10-27 16:39:07 +0300

    add other template

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,9 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
 
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
       </div>
+
+      <EmployeesList />
+      <EmployeesAddForm />
     </div>
   );
 }
